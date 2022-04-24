55c89b1b8941d7233a7a87edb8672ad9daefc90c
Liam Massey <liammassey21@gmail.com>
2022-04-24 17:32:27 +1000

    Add functions to script

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 
 const choices = ['rock', 'paper', 'scissors'];
-let computerSelection = computerPlay();
-let playerSelection = prompt('Choose Rock, Paper, Scissors?');
-console.log(playerSelection);
-
 
 function computerPlay() {
     const selection = choices[Math.floor(Math.random()* choices.length)];
     return selection;
 }
 
+function user() {
+    const userInput = prompt('Choose Rock, Paper, Scissors?');
+    return userInput;
+}
+
+const computerSelection = computerPlay();
+const playerSelection = user();
+
+function playRound(playerSelection, computerSelection) {
+        if ('rock' < 'paper') {
+            return alert('Paper beats Rock, you lose');
+        } else if ('paper' < 'scissors') {
+            return alert('Scissors beats paper, you lose');
+        } else if ('scissors' < 'rock') {
+            return alert('Rock beats Scissors, you lose');
+        } else if ('rock' === 'rock' || 'paper' === 'paper' || 'scissors' === 'scissors') {
+            return alert('It\'s a tie, try again');
+        } else {
+            return 'You win!'
+        }
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
 
-console.log(computerSelection);
