a8a6c5a2199d5af3e971bf28053297f5831148fe
Liam Massey <liammassey21@gmail.com>
2022-04-28 10:16:02 +1000

    Modified and debugged game function

--- script.js
+++ script.js
@@ -14,49 +14,39 @@ function user() {
     return userInput.toLowerCase();   
 }
 
-const playerChoice = user();
-const computerChoice = computerPlay();
 
-let roundCounter = 0;
 
 function playRound(playerSelection, computerSelection) {
        
     let outcome = '';
 
         if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
-            console.log('Paper beats rock, you lose');
             alert('Paper beats rock, you lose');
             computerScore++;
             return outcome = `Paper beats rock, you lose player ${playerScore} computer ${computerScore}`;
         } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
-            console.log('scissors beats paper, you lose');
             alert('scissors beats paper, you lose');
             computerScore++;
             return outcome = `scissors beats paper, you lose player ${playerScore} computer ${computerScore}`
         } else if ((playerSelection === 'scissors')&& (computerSelection === 'rock')) {
-            console.log('rock beats scissors, you lose');
             alert('rock beats scissors, you lose');
             computerScore++;
             return outcome = `rock beats scissors, you lose player ${playerScore} computer ${computerScore}`
         } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
-            console.log('rock beats scissors, you win');
             alert('rock beats scissors, you win');
             playerScore++;
             return outcome = `rock beats scissors, you win player ${playerScore} computer ${computerScore}`
         } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
-            console.log('scissors beats paper, you win');
             alert('scissors beats paper, you win');
             playerScore++;
-            
             return outcome = `scissors beats paper, you win player ${playerScore} computer ${computerScore}`
         } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
-            console.log('paper beats rock, you win');
             alert('paper beats rock, you win');
             playerScore++;
-            return outcome = `paper beats rock, you win player ${playerScore} computer ${computerScore}`
+            return outcome = `paper beats rock, you win player: ${playerScore} computer: ${computerScore}`
         } else {
             alert('its a tie')
-            return outcome = 'its a tie'
+            return outcome = `It's a tie, player: ${playerScore} computer ${computerScore}`;
         }
 } 
 
@@ -73,11 +63,14 @@ function game () {
     }
 
     if (playerScore > computerScore) {
-        console.log( gameOver = `You win`);
+        console.log( gameOver = `You are the winner, you scored ${playerScore}`);
+        alert(`you win player score ${playerScore} computer Score ${computerScore}`);
     } else if (computerScore > playerScore) {
-        console.log(gameOver = `you lose`);
+        console.log(gameOver = `you are the loser, computer scored ${computerScore}`);
+        alert(`you lose player score ${playerScore} computer Score ${computerScore}`);
     } else {
         console.log(`It's a draw`);
+        alert('I don\'t know how to break this to you, but its a tie');
     }
 
     console.log(`${playerScore} ${computerScore}`);
